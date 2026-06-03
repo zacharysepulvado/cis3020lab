e66e22bb5df32afdd28b23270fc6d5dd90f7a4ba
zacharysepulvado <zacharysepulvado@outlook.com>
2026-06-03 16:22:28 -0500

    Carousel array update for more images

--- carousel.js
+++ carousel.js
@@ -1,39 +1,65 @@
 const portfolioImages = [
-  {
-    src: "images/creative-portrait.jpg",
-    caption: "Creative Portraits",
-    alt: "Creative portrait photography example"
-  },
-  {
-    src: "images/creative-portrait-4.jpg",
-    caption: "Creative Portraits",
-    alt: "Creative portrait photography example"
-  },
-  {
-    src: "images/couple-engagement.jpg",
-    caption: "Couples / Engagement",
-    alt: "Couples photography example"
-  },
-  {
-    src: "images/couple-engagement-5.jpg",
-    caption: "Couples / Engagement",
-    alt: "Couples photography example"
-  },
-  {
-    src: "images/graduation-02.jpg",
-    caption: "Graduation Sessions",
-    alt: "Graduation photography example"
-  },
-  {
-    src: "images/graduation-05.jpg",
-    caption: "Graduation Sessions",
-    alt: "Graduation photography example"
-  },
-  {
-    src: "images/graduation-18.jpg",
-    caption: "Graduation Sessions",
-    alt: "Graduation photography example"
-  }
+  "images/creative-portrait.jpg",
+  "images/creative-portrait-2.jpg",
+  "images/creative-portrait-3.jpg",
+  "images/creative-portrait-4.jpg",
+  "images/creative-portrait-5.jpg",
+  "images/creative-portrait-6.jpg",
+  "images/creative-portrait-7.jpg",
+  "images/creative-portrait-8.jpg",
+  "images/creative-portrait-9.jpg",
+  "images/creative-portrait-10.jpg",
+  "images/creative-portrait-11.jpg",
+  "images/creative-portrait-12.jpg",
+  "images/creative-portrait-13.jpg",
+  "images/creative-portrait-14.jpg",
+
+  "images/couple-engagement.jpg",
+  "images/couple-engagement-2.jpg",
+  "images/couple-engagement-3.jpg",
+  "images/couple-engagement-4.jpg",
+  "images/couple-engagement-5.jpg",
+  "images/couple-engagement-6.jpg",
+  "images/couple-engagement-7.jpg",
+  "images/couple-engagement-8.jpg",
+  "images/couple-engagement-09.jpg",
+  "images/couple-engagement-10.jpg",
+
+  "images/headshot.jpg",
+  "images/event.jpg",
+
+  "images/graduation-02.jpg",
+  "images/graduation-03.jpg",
+  "images/graduation-04.jpg",
+  "images/graduation-05.jpg",
+  "images/graduation-06.jpg",
+  "images/graduation-07.jpg",
+  "images/graduation-08.jpg",
+  "images/graduation-09.jpg",
+  "images/graduation-10.jpg",
+  "images/graduation-11.jpg",
+  "images/graduation-12.jpg",
+  "images/graduation-13.jpg",
+  "images/graduation-14.jpg",
+  "images/graduation-15.jpg",
+  "images/graduation-16.jpg",
+  "images/graduation-17.jpg",
+  "images/graduation-18.jpg",
+  "images/graduation-19.jpg",
+  "images/graduation-20.jpg",
+  "images/graduation-21.jpg",
+  "images/graduation-22.jpg",
+  "images/graduation-23.jpg",
+  "images/graduation-24.jpg",
+  "images/graduation-25.jpg",
+  "images/graduation-26.jpg",
+  "images/graduation-27.jpg",
+  "images/graduation-28.jpg",
+  "images/graduation-29.jpg",
+  "images/graduation-30.jpg",
+  "images/graduation-31.jpg",
+  "images/graduation-32.jpg",
+  "images/graduation-33.jpg"
 ];
 
 const carouselImage = document.getElementById("carousel-image");
